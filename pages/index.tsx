692fbd71a24d4492cbd01a813effb52d5350e2f2
York Ng <yorkhayng@gmail.com>
2024-12-27 01:40:45 +0800

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { Moon, MoonIcon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Toggle } from "@/components/ui/toggle"
+
+
+export default function Home() {
+  const { setTheme } = useTheme()
+  return (
+    <div className='flex bg-background text-foreground h-screen'>
+      <Toggle onClick={() => setTheme(theme => theme === 'dark' ? 'light' : 'dark')}>
+        <MoonIcon />
+      </Toggle>
+    </div>
+  );
+}
